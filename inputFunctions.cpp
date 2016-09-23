bb9f3bc4f3719393831dabd6a25331c107fe3399
JosephReeves6 <josephreeves@mail.fresnostate.edu>
2016-09-22 19:30:36 -0700

    Add files via upload

---
+++ inputFunctions.cpp
@@ -0,0 +1,51 @@
+/*
+ * file containing the functions for user commands
+ *
+ * #include Spot.h //this will allow you to access the member functions from the Spot class
+ * #include Car.h //this will allow you to access the Car class
+ *
+ * the class name is Spot, and the object being passed to 
+ * each function is labeled as x
+ */
+
+
+/*
+
+  returns the next available spot
+  or 0 if there are no spots available 
+
+*/
+int getSpot(Spot x, Car y)
+{
+  //function code
+}
+
+
+
+
+/*
+  
+ input is an integer to represent the spot that is being left
+
+ returns 1 to confirm that the person is leaving a valid spot
+ returns 0 if it was not a valid spot
+
+*/
+bool leaveSpot(Spot x, int leftSpot)
+{
+  //function code
+}
+
+
+
+/*
+
+no input or output
+
+prints out all of the currently available spots in the lot
+
+*/
+void displayAll(Spot x)
+{
+  //function code
+}
