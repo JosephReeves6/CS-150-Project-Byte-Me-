e3de22fa9ca3611dc754eeb9425f8f968f97ab6a
JosephReeves6 <josephreeves@mail.fresnostate.edu>
2016-09-29 14:57:39 -0700

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -22,15 +22,24 @@ using namespace std;
 int main(void)
 {
     //initialize Spot object
-    
-    //initialize queues
-    
-    //start taking input for user
+ 
+    while (true)
+    {
+     
+    //start taking input from user, or allow input to end entire program
     //first ask for name, type of car (normal, handicap...), and size (1 = compact, 2 = normal, 3 = oversized)
-    //record input into new car object
+    //record input into new dynamically allocated car object
     
+ 
+    while (true)
+    {
     
-    //ask if they want to get a spot, leave, or display all open
+    //ask if they want to get a spot, leave, or display all open, or exit the program
     //call a function from inputFunctions.cpp depending on which option they choose and take more input as needed
+    //or break from the loop
+     
+    } //end of inner while
+     
+    } //end of outer while
 
 }
