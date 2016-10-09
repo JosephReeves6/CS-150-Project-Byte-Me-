d95b7d8b6c623c5270fcb365caec275cb3ed9c07
frankiem3 <frankieavina@mail.fresnostate.edu>
2016-10-08 21:26:53 -0700

    Update inputFunctions.cpp

--- CPS/BLL/inputFunctions.cpp
+++ CPS/BLL/inputFunctions.cpp
@@ -1,20 +1,23 @@
 /*
  * file containing the functions for user commands
- *
- * #include Spot.h //this will allow you to access the member functions from the Spot class
- * #include Car.h //this will allow you to access the Car class
- *
- * the class name is Spot, and the object being passed to 
+ */
+ #include "Spot.h" //this will allow you to access the member functions from the Spot class
+ #include "Car.h" //this will allow you to access the Car class
+ #include <vector>
+ #include <iterator>
+ #include <list>
+ #include "tinyxm12.h"
+ /* the class name is Spot, and the object being passed to 
  * each function is labeled as x
  */
 
+using namespace std;
+using namespace tinyxm12; 
+static const char* path = "DAL/ParkingStrucute.xml";
 
-/*
 
-  returns the next available spot
-  or 0 if there are no spots available 
-
-*/
+/*returns the next available spot or 0 if there are no spots available */
+/*************************fucntion that gets a spot for the user ***********************************/
 int getSpot(Spot x, Car y)
 {
   //function code
@@ -23,14 +26,9 @@ int getSpot(Spot x, Car y)
 
 
 
-/*
-  
- input is an integer to represent the spot that is being left
-
- returns 1 to confirm that the person is leaving a valid spot
- returns 0 if it was not a valid spot
-
-*/
+/*input is an integer to represent the spot that is being left returns 
+1 to confirm that the person is leaving a valid spot returns 0 if it was not a valid spot*/
+/***************************************** fucntion to leave a spot **********************************/
 bool leaveSpot(Spot x, int leftSpot)
 {
   //function code
@@ -38,14 +36,38 @@ bool leaveSpot(Spot x, int leftSpot)
 
 
 
-/*
-
-no input or output
-
-prints out all of the currently available spots in the lot
-
-*/
+/****************prints out all of the currently available spots in the lot**************************/
 void displayAll(Spot x)
 {
-  //function code
+  XMLDocument dataStruc;
+  MakeConnection(dataStruc);
+  
+  
+  XMLNode * pRoot = dataStruc.FirstChild();
+  XMLElement * pListElement = pRoot -> FristChildElement("Spot"); 
+  list<Spot> spotsList;
+  cont char * xmlID = 0;
+  const char * xmlStatus = 0;
+  
+   cout<<"*********************************************************"<<endl; 
+   cout<<"****************  PARKING SPOT LIST  ********************"<<endl;
+   cout<<"*********************************************************"<<endl;
+  
+  while ( pListElement != 0 ){
+    
+    xmlID = pListElement -> Attribute("ID");
+    if( xmlID == 0 ) cout << XML_ERROR_PASSING_ATTRIBUTE;
+    int id = std::atoi (xmlID);
+    
+    xmlStatus = pListElement -> Attribute("Status");
+    if( xmlStatus == 0 ) cout << XML_ERROR_PASSING_ATTRIBUTE;
+    int status = std::atoi (xmlStatus);
+    
+    cout<<"ID: "<< id <<"\tStatus: "<< status <<endl; 
+    spotsList.push_back(p); 
+    
+    pListElement = pListElement -> NextSiblingElement("Spots"); 
+  }
+    return spotsList;
 }
+}// end of display all 
